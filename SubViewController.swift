a3c9ddd3852969d9e3e249327f3865a1e5b604d0
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2020-04-02 15:59:47 +0700

    add gitignore of swift

---
+++ SubViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SubViewController.swift
+//  AutoLayoutUsingActivate
+//
+//  Created by Phatcharaphan Ananpreechakun on 2/4/20.
+//  Copyright © 2020 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
